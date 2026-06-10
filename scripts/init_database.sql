1fb012358c3d920068b3082d0ea5c80965b7abc0
Manish Kumar <66414715+kumarmannish@users.noreply.github.com>
2026-06-10 15:18:17 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -12,31 +12,39 @@ WARNING:
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
 */
-
 USE master;
-GO
 
+
+GO
 -- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
-BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
-END;
-GO
+IF EXISTS (SELECT 1
+           FROM   sys.databases
+           WHERE  name = 'DataWarehouse')
+    BEGIN
+        ALTER DATABASE DataWarehouse
+            SET SINGLE_USER 
+            WITH ROLLBACK IMMEDIATE;
+        DROP DATABASE DataWarehouse;
+    END
 
+
+GO
 -- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
-GO
 
+
+GO
 USE DataWarehouse;
-GO
 
+
+GO
 -- Create Schemas
 CREATE SCHEMA bronze;
-GO
 
+
+GO
 CREATE SCHEMA silver;
-GO
 
-CREATE SCHEMA gold;
+
 GO
+CREATE SCHEMA gold;
